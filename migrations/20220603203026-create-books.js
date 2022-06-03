fe8e33bf547a115374938478412143bc0d8d58cf
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-06-03 17:43:31 -0300

    migration + model books

---
+++ migrations/20220603203026-create-books.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Books', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      edition_year: {
+        type: Sequelize.INTEGER
+      },
+      author: {
+        type: Sequelize.STRING
+      },
+      pages: {
+        type: Sequelize.INTEGER
+      },
+      stock: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Books');
+  }
+};
