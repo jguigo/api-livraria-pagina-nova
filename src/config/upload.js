63ed507598a91fb8027363ef1249ebf31f35b677
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-06-23 21:09:17 -0300

    configuração do upload de imagens

--- src/config/upload.js
+++ src/config/upload.js
@@ -1,11 +1,17 @@
 const multer = require("multer");
+const path = require("path");
 
 const storage = multer.diskStorage({
    destination: function (req, file, cb) {
-      cb(null, "/tmp/my-uploads");
+      cb(null, path.resolve("src/public", "images"));
    },
    filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-      cb(null, file.fieldname + "-" + uniqueSuffix);
+      cb(null, Date.now() + "-" + file.originalname);
    },
 });
+
+const upload = multer({
+   storage: storage,
+});
+
+module.exports = upload;
