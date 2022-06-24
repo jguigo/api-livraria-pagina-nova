7acf3b3ebe0fbd16bc9fee5637d7823379bbfd21
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-06-23 21:09:48 -0300

    configuração do para funcionar o upload de imagens

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const routes = require("./routes");
 const db = require("./database");
+const upload = require("./config/upload");
 
 const app = express();
 
@@ -8,6 +9,8 @@ db.hasConection();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(upload.any());
+app.use(express.static('src/public/images'));
 
 app.use(routes);
 
