86fcd2926aa2043b9896ea455f631813e82f27de
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-06-25 11:04:24 -0300

    salvando o endereço estatico na núvem

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,12 +1,17 @@
-const { Books } = require('../database/models/index');
+const { Books } = require("../database/models/index");
+
+const imageFolder = "image/";
 
 const BookController = {
    async create(req, res) {
+      const file = req.files[0];
       const newBook = await Books.create({
-         ...req.body
+         ...req.body,
+         image: imageFolder + file.filename,
       });
+      console.log(req.files);
       return res.status(201).json(newBook);
-   }
+   },
 };
 
 module.exports = BookController;
