2b4c41afd50766ed0b08fe4b3af87942cb48ca9b
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-06-25 10:57:26 -0300

    adicionado campo de image

--- src/database/models/books.js
+++ src/database/models/books.js
@@ -2,41 +2,44 @@ const db = require("..");
 const { DataTypes } = require("sequelize");
 
 const Books = db.define(
-  "Books",
-  {
-    id: {
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-      type: DataTypes.INTEGER
-    },
-    title: {
-      type: DataTypes.STRING
-    },
-    edition_year: {
-      type: DataTypes.INTEGER
-    },
-    author: {
-      type: DataTypes.STRING
-    },
-    pages: {
-      type: DataTypes.INTEGER
-    },
-    stock: {
-      type: DataTypes.INTEGER
-    },
-    createdAt: {
-      allowNull: true,
-      type: DataTypes.DATE
-    },
-    updatedAt: {
-      allowNull: true,
-      type: DataTypes.DATE
-    }
-  },
-  {
-    tableName: "books",
-  }
+   "Books",
+   {
+      id: {
+         allowNull: false,
+         autoIncrement: true,
+         primaryKey: true,
+         type: DataTypes.INTEGER,
+      },
+      title: {
+         type: DataTypes.STRING,
+      },
+      edition_year: {
+         type: DataTypes.INTEGER,
+      },
+      author: {
+         type: DataTypes.STRING,
+      },
+      pages: {
+         type: DataTypes.INTEGER,
+      },
+      stock: {
+         type: DataTypes.INTEGER,
+      },
+      image: {
+         type: DataTypes.STRING,
+      },
+      createdAt: {
+         allowNull: true,
+         type: DataTypes.DATE,
+      },
+      updatedAt: {
+         allowNull: true,
+         type: DataTypes.DATE,
+      },
+   },
+   {
+      tableName: "books",
+   }
 );
 
 module.exports = Books;
