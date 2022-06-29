8ccb8747ad400298540169f11e926a47016085f2
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-06-29 20:38:03 -0300

    feat: adding db env to sequelize-cli config

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -9,17 +9,17 @@ module.exports =
     "dialect": process.env.DB_DIALECT
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_DIALECT
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_DIALECT
   }
 }
